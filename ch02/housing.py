3ea31926e8429c46fa25cbf2f6a2b8a8f5cf8323
Yefan Zhang <yzhang@bluehole.net>
2020-01-08 18:40:35 +0100

    page 67

--- ch02/housing.py
+++ ch02/housing.py
@@ -109,3 +109,166 @@ housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
 plt.legend()
 
 # %%
+corr_matrix = housing.corr()
+corr_matrix["median_house_value"].sort_values(ascending=False)
+
+# %%
+from pandas.plotting import scatter_matrix
+
+attributes = ["median_house_value", "median_income", "total_rooms", "housing_median_age"]
+scatter_matrix(housing[attributes], figsize=(12, 8))
+
+# %%
+housing.plot(kind="scatter", x="median_income", y="median_house_value", alpha=0.1)
+
+# %%
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"]=housing["population"]/housing["households"]
+
+corr_matrix = housing.corr()
+corr_matrix["median_house_value"].sort_values(ascending=False)
+
+# %%
+housing = strat_train_set.drop("median_house_value", axis=1)
+housing_labels = strat_train_set["median_house_value"].copy()
+housing.describe()
+
+# %%
+from sklearn.preprocessing import Imputer
+
+housing_num = housing.drop("ocean_proximity", axis=1)
+imputer = Imputer(strategy="median")
+imputer.fit(housing_num)
+
+# %%
+imputer.statistics_
+
+# %%
+housing_num.median().values
+
+# %%
+X = imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=housing.index)
+
+#%%
+housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=housing_num.index)
+housing_tr.head()
+
+#%%
+housing_cat = housing[['ocean_proximity']]
+housing_cat.head(10)
+
+#%%
+from sklearn.preprocessing import OrdinalEncoder
+
+ordinal_encoder = OrdinalEncoder()
+housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+housing_cat_encoded[:10]
+
+#%%
+ordinal_encoder.categories_
+
+#%%
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+cat_encoder = OneHotEncoder()
+housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
+housing_cat_1hot
+
+#%%
+housing_cat_1hot.toarray()
+
+#%%
+housing.columns
+
+# %%
+# from sklearn.preprocessing import LabelEncoder
+
+# encoder = LabelEncoder()
+# housing_cat = housing["ocean_proximity"]
+# housing_cat_encoded = encoder.fit_transform(housing_cat)
+# housing_cat_encoded
+
+# %%
+# print(encoder.classes_)
+
+# %%
+# from sklearn.preprocessing import OneHotEncoder
+
+# encoder = OneHotEncoder()
+# housing_cat_1hot = encoder.fit_transform(housing_cat_encoded.reshape(-1, 1))
+# housing_cat_1hot
+
+# %%
+# housing_cat_1hot.toarray()
+
+# %%
+# from sklearn.preprocessing import LabelBinarizer
+# encoder = LabelBinarizer()
+# housing_cat_1hot = encoder.fit_transform(housing_cat)
+# housing_cat_1hot
+
+# %%
+from sklearn.base import BaseEstimator, TransformerMixin
+
+rooms_ix, bedrooms_ix, population_ix, household_ix = [
+    list(housing.columns).index(col)
+    for col in ("total_rooms", "total_bedrooms", "population", "households")]
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room=True):
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X, y=None):
+        rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+        population_per_household = X[:, population_ix] / X[:, household_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household,
+                         bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
+
+#%%
+housing_extra_attribs = pd.DataFrame(
+    housing_extra_attribs,
+    columns=list(housing.columns)+["rooms_per_household", "population_per_household"],
+    index=housing.index)
+housing_extra_attribs.head()
+
+# %%
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+num_pipeline = Pipeline([
+    ('imputer', Imputer(strategy="median")),
+    ('attribs_adder', CombinedAttributesAdder()),
+    ('std_scaler', StandardScaler()),])
+
+housing_num_tr = num_pipeline.fit_transform(housing_num)    
+housing_num_tr
+
+# %%
+from sklearn.compose import ColumnTransformer
+
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+full_pipeline = ColumnTransformer([
+    ("num", num_pipeline, num_attribs),
+    ("cat", OneHotEncoder(), cat_attribs)])
+
+housing_prepared = full_pipeline.fit_transform(housing)
+housing_prepared
+
+# %%
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit()
