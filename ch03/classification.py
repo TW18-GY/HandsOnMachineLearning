079d8660f526fb44ae1774572f955c3371e32229
Yefan Zhang <yzhang@bluehole.net>
2020-01-13 17:39:13 +0100

    page 167

--- ch03/classification.py
+++ ch03/classification.py
@@ -104,3 +104,92 @@ recall_score(y_train_5, y_train_pred)
 
 # %%
 from sklearn.metrics import f1_score
+
+f1_score(y_train_5, y_train_pred)
+
+# %%
+y_scores = sgd_clf.decision_function([some_digit])
+y_scores
+
+# %%
+threshold = 0
+y_some_digit_pred = (y_scores > threshold)
+y_some_digit_pred
+
+# %%
+y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,
+                             method="decision_function")
+
+# %%
+from sklearn.metrics import precision_recall_curve
+
+precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+
+# %%
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
+    plt.xlabel("Threshold", fontsize=16)
+    plt.legend(loc="upper left", fontsize=16)
+    plt.ylim([0, 1])
+
+plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+plt.show()
+
+# %%
+threshold_90_precision = thresholds[np.argmax(precisions >= 0.90)]
+y_train_pred_90 = (y_scores >= threshold_90_precision)
+
+# %%
+precision_score(y_train_5, y_train_pred_90)
+
+# %%
+recall_score(y_train_5, y_train_pred_90)
+
+# %%
+from sklearn.metrics import roc_curve
+
+fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+
+# %%
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel('False Positive Rate', fontsize=16)
+    plt.ylabel('True Positive Rate', fontsize=16)
+
+plt.figure(figsize=(8,6))    
+plot_roc_curve(fpr, tpr)
+plt.show()
+
+# %%
+from sklearn.ensemble import RandomForestClassifier
+
+forest_clf = RandomForestClassifier(random_state=42)
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5,
+                                    cv=3, method="predict_proba")
+
+# %%
+y_scores_forest = y_probas_forest[:, 1]
+fpr_forest, tpr_forest, threshold_forest = roc_curve(y_train_5, y_scores_forest)
+
+# %%
+plt.plot(fpr, tpr, "b:", label="SGD")
+plot_roc_curve(fpr_forest, tpr_forest, "Random Forest")
+plt.legend(loc="lower right")
+plt.show()
+
+# %%
+from sklearn.metrics import roc_auc_score
+
+roc_auc_score(y_train_5, y_scores_forest)
+
+# %%
+from sklearn.svm import SVC
+
+svm_clf = SVC()
+svm_clf.fit(X_train, y_train)
+svm_clf.predict([some_digit])
+
+# %%
